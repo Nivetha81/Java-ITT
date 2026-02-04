c6b71329b23e9e308712eafe18d0731aae0176a5
Nivetha S <nivetha04.nivi@gmail.com>
2026-02-04 10:30:43 +0530

    Update inheritance2.java

--- inheritance2.java
+++ inheritance2.java
@@ -8,20 +8,12 @@ class Arithmetic{
     return a+b;
 }
 }
-class Adder extends Arithmetic{
-    
+class Adder extends Arithmetic{    
 }
-//Write your code here
-
 public class Solution{
     public static void main(String []args){
-        // Create a new Adder object
-        Adder a = new Adder();
-        
-        // Print the name of the superclass on a new line
+        Adder a = new Adder();        
         System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());	
-        
-        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
         System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
      }
 }
